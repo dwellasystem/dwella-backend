8e540cbfb19f240503a93018a9f93b6febac19cf
eimanncalderon0711 <calderon.eimann0711@gmail.com>
2025-12-07 12:54:46 +0800

    new updates

--- api/payments/urls.py
+++ api/payments/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('payment-method/<int:pk>/delete/', views.delete_payment_method_by_id, name='delete_payment_method_by_id'),
 
     path('payments', PaginatedPayments.as_view(), name='paginated-payments' ),
-    # path('payments', views.get_payments, name='get_payments'),
+    path('allpayments', views.get_payments, name='get_payments'),
     path('payment/', views.create_payment, name='create_payment'),
     path('payment/<int:pk>/', views.get_payment_by_id, name='get_payment_by_id'),
     path('payment/<int:pk>/update/', views.update_payment_by_id, name='update_payment_by_id'),
